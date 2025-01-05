81d1483f43761edc248f52e9fa5e43736ceb295e
Edward Gough <edwardggough@gmail.com>
2025-01-05 13:45:40 +0000

    Godo logo added to navigation and file system.
    Background updated to follow larger content.
    
    Editable list titles and remove button styled.
    
    Updated mobile formatting, but further improvements need to be added.

--- resources/views/lists.blade.php
+++ resources/views/lists.blade.php
@@ -16,7 +16,8 @@
         <!-- Navigation -->
         <nav>
             <div class="inner">
-                <h1>Godo</h1>
+                <h1 style="display:none;">Godo</h1>
+                <img src="{{ asset('media/download/godo-logo.png') }}" alt="">
             </div>
         </nav>
 
@@ -24,11 +25,16 @@
 
         <!-- Container of List Category -->
         <div class="lists">
+            
+            <!-- Default List -->
             <div class="inner">
 
                 <!-- Header of List Category -->
                 <header>
-                    <h2>Default List</h2>
+                    <h2 contenteditable>Default List</h2>
+                    <a class="button remove" href="">
+                        Remove
+                    </a>
                 </header>
 
                 <!-- Container for each list item -->
@@ -52,11 +58,11 @@
                             </div>
                         </div>
                         <div class="list-actions">
-                            <button class="action delete">
+                            <a class="action delete">
                                 <span class="material-symbols-outlined">
                                     close
                                 </span>
-                            </button>
+                            </a>
                         </div>
                     </div>
 
@@ -78,17 +84,88 @@
                             </div>
                         </div>
                         <div class="list-actions">
-                            <button class="action delete">
+                            <a class="action delete">
                                 <span class="material-symbols-outlined">
                                     close
                                 </span>
-                            </button>
+                            </a>
                         </div>
                     </div>
 
                 </div>
 
             </div>
+
+            <!-- Secondary List -->
+            <div class="inner">
+
+                <!-- Header of List Category -->
+                <header>
+                    <h2 contenteditable>Household</h2>
+                    <a class="button remove" href="">
+                        Remove
+                    </a>
+                </header>
+
+                <!-- Container for each list item -->
+                <div class="list">
+                   
+                    <!-- Single List Item -->
+                    <div class="list-item">
+                        <div class="list-icon">
+                            <span class="material-symbols-outlined">
+                                check_box_outline_blank
+                            </span>
+                        </div>
+                        <div class="list-content">
+                            <h3 contenteditable>Task 2</h3>
+                            <p contenteditable>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie, felis id commodo posuere, lorem ligula dignissim justo, quis cursus nisl velit vestibulum nisl. Vestibulum tristique finibus justo, sed placerat magna placerat a. In dictum aliquam nibh in vehicula. Etiam a suscipit dolor, non fermentum justo. Morbi laoreet purus libero, ut gravida ipsum malesuada eget.</p>
+                            <div class="list-additional">
+                                <span class="material-symbols-outlined">
+                                    calendar_today
+                                </span>
+                                <h4>04/01/2025</h4>
+                            </div>
+                        </div>
+                        <div class="list-actions">
+                            <a class="action delete">
+                                <span class="material-symbols-outlined">
+                                    close
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+
+                    <!-- Closed Single List Item -->
+                    <div class="list-item closed">
+                        <div class="list-icon">
+                            <span class="material-symbols-outlined">
+                                check_box
+                            </span>
+                        </div>
+                        <div class="list-content">
+                            <h3 contenteditable>Task 1</h3>
+                            <p contenteditable>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam molestie, felis id commodo posuere, lorem ligula dignissim justo, quis cursus nisl velit vestibulum nisl. Vestibulum tristique finibus justo, sed placerat magna placerat a. In dictum aliquam nibh in vehicula. Etiam a suscipit dolor, non fermentum justo. Morbi laoreet purus libero, ut gravida ipsum malesuada eget.</p>
+                            <div class="list-additional">
+                                <span class="material-symbols-outlined">
+                                    calendar_today
+                                </span>
+                                <h4>02/01/2025</h4>
+                            </div>
+                        </div>
+                        <div class="list-actions">
+                            <a class="action delete">
+                                <span class="material-symbols-outlined">
+                                    close
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
         </div>
 
 
@@ -96,7 +173,6 @@
         <!-- Container QR Code -->
         <div class="qrcode-container">
             <div class="inner">
-                <h3>Scan the code to access from anywhere</h3>
                 <img src="{{ asset('media/download/example-qrcode.png') }}" alt="QR Code">
             </div>
         </div>
